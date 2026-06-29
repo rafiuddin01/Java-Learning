e7c6679a48959da49029347b92c91dbdd2354da8
rafiuddin01 <rafiuddin0504@gmail.com>
2026-06-29 22:22:53 +0530

    nestedif example

---
+++ Day-2/nestedif.java
@@ -0,0 +1,13 @@
+public class nestedif {
+    public static void main(String[] args) {
+        int age = 22;
+boolean hasLicense = true;
+
+if (age >= 18) {
+    if (hasLicense) {
+        System.out.println("Can Drive");
+    }
+}
+        }
+    }
+    
