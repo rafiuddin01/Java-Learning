d6a96b9abf8b084b3baa666ca2768b5a35d6158f
rafiuddin01 <rafiuddin0504@gmail.com>
2026-07-16 21:19:28 +0530

    assignment1

---
+++ Day-4/DataTypes.java
@@ -0,0 +1,11 @@
+public class DataTypes {
+
+	public static void main(String[] args) {
+		// Implement your code here 
+		double pi = 3.14;
+		int radius = 5;
+		double area = pi*radius*radius;
+		System.out.println("area of the circle" + area);
+	}
+
+}
---
+++ Day-4/assignment.java
@@ -0,0 +1,12 @@
+public class assignment {
+
+    public static void main(String[] args) {
+
+        double fahrenheit = 98;
+
+        double celsius = (fahrenheit - 32) * 5 / 9;
+
+        System.out.println("Temperature in Celsius = " + celsius);
+
+    }
+}
