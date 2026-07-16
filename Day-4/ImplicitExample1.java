083c1157091dbc1fc7a924144fb7ecf25a500d14
rafiuddin01 <rafiuddin0504@gmail.com>
2026-07-16 19:58:15 +0530

    implicitexample

---
+++ Day-4/ImplicitExample1.java
@@ -0,0 +1,12 @@
+public class ImplicitExample1 {
+    public static void main(String[] args) {
+
+        int number = 100;
+
+        double value = number;
+
+        System.out.println("Integer Value : " + number);
+        System.out.println("Double Value  : " + value);
+
+    }
+}
