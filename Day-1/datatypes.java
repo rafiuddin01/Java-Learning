f3b792dfbe5901eea4d35cbb016cf68eb7e46ac2
rafiuddin01 <rafiuddin0504@gmail.com>
2026-07-18 23:54:24 +0530

    code changes

--- Day-1/datatypes.java
+++ Day-1/datatypes.java
@@ -1,11 +1,11 @@
 public class datatypes {
     public static void main(String[] args) {
-        int num = 100;
+        int ann = 100;
         double price = 99.99;
         char grade = 'A';
         boolean status = true;
 
-        System.out.println(num);
+        System.out.println(ann);
         System.out.println(price);
         System.out.println(grade);
         System.out.println(status);
--- Day-4/typecoversion.java
+++ Day-4/typecoversion.java
@@ -5,23 +5,12 @@ class Tester {
 		float variableTwo = variableOne;
 		System.out.println(variableOne);
 		System.out.println(variableTwo);
-		// Here, a variable of data type having smaller range is automatically
-		// accommodated in a variable of data type having bigger range. This is
-		// known as widening.
+		
 
-		// When we try to accommodate a variable of data type having bigger
-		// range into a data type having smaller range, it leads to an error.
 		float variableThree = 12.5f;
-		int variableFour = (int) variableThree; // comment this line or fix the error
-											// to execute the code successfully
+		int variableFour = (int) variableThree;
 		System.out.println(variableThree);
-		System.out.println(variableFour); // comment this line or fix the error
-											// to execute the code successfully
-
-		// Since our assignment is leading to loss of data, we need to
-		// explicitly specify that the data needs to be converted to a data type
-		// having smaller range.
-		// This is known as explicit typecasting.
+		System.out.println(variableFour); 
 		int variableFive = (int) variableThree;
 		System.out.println(variableFive);
 	}
