8c42cf58d41a05f5ea7be892e873d0b099dd77d4
rafiuddin01 <rafiuddin0504@gmail.com>
2026-07-18 23:22:31 +0530

    switch example

---
+++ Day-5/switchCaseExample.java
@@ -0,0 +1,18 @@
+public class switchCaseExample {
+	public static void main(String[] args) {
+		String orderedFood = "Pizza";
+		switch (orderedFood) {
+		case "Burger":
+			System.out.println("You have ordered Burger. Unit price: $10");
+			break;
+		case "Pizza":
+			System.out.println("You have ordered Pizza. Unit price: $15");
+			break;
+		case "Sandwich":
+			System.out.println("You have ordered Sandwich. Unit price: $8");
+			break;
+		default:
+			System.out.println("Invalid selection");
+		}
+	}
+}
