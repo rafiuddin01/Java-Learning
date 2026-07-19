9b2e731a235e9af1a256331db468f79ae7bc3b06
rafiuddin01 <rafiuddin0504@gmail.com>
2026-07-19 20:04:35 +0530

    datatypes and operators assignment

---
+++ Day-7/assignment2.java
@@ -0,0 +1,14 @@
+class Tester {
+	public static void main(String[] args) {
+		// Implement your code here 
+        double doorNo = 89;
+        String street = "St. Louis Street";
+        String city = "Springfield";
+        String zipCode = "62729";
+
+		System.out.println("Door No: D" + (int)doorNo);
+		System.out.println("Street: " + street);
+		System.out.println("City: " + city);
+		System.out.println("ZIP Code: " + zipCode);
+	}
+}
