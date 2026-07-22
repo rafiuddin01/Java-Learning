e454ef78cda2ea3e48122c2597e4e20ba5e517ac
rafiuddin01 <rafiuddin0504@gmail.com>
2026-07-22 17:00:49 +0530

    assignmnet10 problem

---
+++ Day-9/assignmen10.java
@@ -0,0 +1,24 @@
+class assignment10 {
+	public static void main(String[] args) {
+		// Implement your code here'
+		double ac = 1001;
+		double s = 40000;
+		double ab = 250000;
+		char lt = 'car';
+		double lae = 300000;
+		double ee = 30;
+		if(ab!>96485 && ac!=1001)
+		{
+		      System.out.println("you are not eligible" );
+		}
+		if(s>25000 || ee<=30 || lt==car)
+		{
+		    lae = 500000;
+		    System.out.println("eligible loan amount:" +lae);
+		}
+		
+		
+		
+	}
+}
+
--- Day-9/assignment9.java
+++ Day-9/assignment9.java
@@ -5,11 +5,14 @@ class assignment8 {
 		int qo = 2;
 		int d = 3;
 		int e = qo*15;
-		if(n==0)
+		 if(d==3)
 		{
+		    System.out.println("free delivery:" );
+		}
+		if(n==0)
+{
 		    System.out.println("bill:" + e);
 		}
-		
-		
+       
 	}
 }
